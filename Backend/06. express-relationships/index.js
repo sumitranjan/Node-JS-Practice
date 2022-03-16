7573e121489265e62851b833af60b04dffacd806
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-03-17 03:07:01 +0530

    remove connection string

--- Backend/06. express-relationships/index.js
+++ Backend/06. express-relationships/index.js
@@ -7,7 +7,8 @@ app.use(express.json());
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-atlas?retryWrites=true&w=majority"
+    //Wrte mongoDB connection string
+      //""
   );
 };
 
--- Backend/07. express-mvc/practice/src/configs/db.js
+++ Backend/07. express-mvc/practice/src/configs/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     return mongoose.connect(
-      "mongodb+srv://sumit:sumit1234@cluster0.wdhhx.mongodb.net/myDBAtlas?authSource=admin&replicaSet=atlas-10rf3t-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+      //Wrte mongoDB connection string
+      //""
     );
   };
 
