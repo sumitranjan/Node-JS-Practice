f53ab366130aafd0fcda42e2f8b900ed0a14a201
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-04-29 00:54:50 +0530

    minor changes

--- React/02.react-intro/src/App.js
+++ React/02.react-intro/src/App.js
@@ -1,6 +1,27 @@
+import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>
+  const food = "Pizza";
+  const age = 19;
+  const skills = ["mern", "node", "dsa", "design", "animation"];
+
+  return (
+    <div className="App">
+      Your fav food is {food}
+      <div>you are {age > 18 ? "Eligible" : "Not eligible"} for license </div>
+      <div>
+        {skills.map((e) => {
+          return <Skill skill={e} key="value" age={12} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+//Component
+function Skill(props) {
+  console.log("Skills props", props);
+  return <p className="redtext">Skill: {props.skill}</p>;
 }
 
 export default App;
