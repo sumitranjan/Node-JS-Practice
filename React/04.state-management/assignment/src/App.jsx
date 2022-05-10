f26e040051e7d7bd584acbd672d7cf4589190fa9
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-05-10 23:03:18 +0530

    to do react

---
+++ React/04.state-management/assignment/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {Todo} from "./components/Todo"
+function App() {
+  
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  )
+}
+
+export default App
