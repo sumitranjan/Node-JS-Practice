29228b877e41498980843d9dda19f535fc8796ec
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-05-18 19:31:15 +0530

    nav-bar complete

---
+++ React2/01.React-intro/assignment/navbar/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Logo from "./components/Logo";
+import Links from "./components/Links";
+import Button from "./components/Button";
+
+function App() {
+  const links = ["Services", "Projects", "About"];
+  return (
+    <div className="App">
+      <div className="nav-bar">
+        <Logo />
+        <div className="link-bar">
+          {links.map((link) => (
+            <div>
+              <Links value={link} />
+            </div>
+          ))}
+        </div>
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ React2/01.React-intro/assignment/navbar/src/components/Links.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Links = (link) => {
+  return (
+    <div>{link.value}</div>
+  )
+}
+
+export default Links
