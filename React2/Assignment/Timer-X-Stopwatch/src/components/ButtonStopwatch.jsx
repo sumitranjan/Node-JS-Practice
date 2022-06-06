3109ef086e435bef5ac2d565b7dcc134d7e04afa
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-06-06 21:33:13 +0530

    stopwatch

---
+++ React2/Assignment/Timer-X-Stopwatch/src/components/ButtonStopwatch.jsx
@@ -0,0 +1,62 @@
+import { Button, Stack, Toast, useToast } from "@chakra-ui/react";
+import React from "react";
+
+const ButtonStopwatch = (props) => {
+    const toast = useToast()
+  console.log(props);
+  return (
+    <div>
+      {props.status == 0 ? (
+        <Button colorScheme="teal" variant="solid" onClick={props.start}>
+          Start
+        </Button>
+      ) : (
+        ""
+      )}
+      {props.status === 1 ? (
+        <div>
+          <Button colorScheme="red" variant="solid" onClick={props.stop}>
+            Stop
+          </Button>
+          <Button colorScheme="gray" variant="solid" onClick={props.reset}>
+            Reset
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {props.status === 2 ? (
+        <div>
+          <Button colorScheme="teal" variant="solid" onClick={props.resume}>
+            Resume
+          </Button>
+          <Button colorScheme="red" variant="solid" onClick={props.reset}>
+            Reset
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <br />
+      <br />
+
+      <Button
+      onClick={() =>
+        toast({
+          title: 'Stopwatch Developed By',
+          description: "Kiran Kamble WEB-17 Masai School",
+          status: 'success',
+          duration: 8000,
+          isClosable: true,
+        })
+      }
+    >
+      Show Credits
+    </Button>
+    </div>
+  );
+};
+
+export default ButtonStopwatch;
