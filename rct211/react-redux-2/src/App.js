872d017dae0563aedcafa3168ed066ec8b3f6c36
sumitranjan <sumitranjan.ind@gmail.com>
2022-07-07 12:55:52 +0530

    update

---
+++ rct211/react-redux-2/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Pages from './pages/Pages';
+// import Navbar from './pages/Navbar';
+// import Todos from './components/Todos';
+// import { Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     <Pages/>
+      
+    </div>
+  );
+}
+
+export default App;
